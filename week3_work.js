db3f83658c34b8d4478c75bbdc0df932ce852007
Adewuyi Adeshina <adewuyi.aadeshina@gmail.com>
2024-04-27 07:49:48 -0700

    week3_work.js

---
+++ week3_work.js
@@ -0,0 +1,19 @@
+let numbers = [1,2,3,4,5];
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+let day = "Monday" ;
+switch(day) {
+    case "Monday" :
+        console.log("It's Monday");
+        break;
+        case "It's Tuesday" :
+            console.log("It's Tuesday");
+            break;
+            case "Wednessday" :
+                console.log("It's Wednessday");
+                break;
+                default :
+                console.log("It's another day");
+}
